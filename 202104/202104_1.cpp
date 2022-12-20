ca25f47e41c4e039d45c45c65f3a32489ade34eb
rucdatascience <87228246+rucdatascience@users.noreply.github.com>
2022-12-20 14:54:06 +0800

    1

---
+++ 202104/202104_1.cpp
@@ -0,0 +1,30 @@
+﻿/* CCF202104-1 灰度直方图 */
+#include <bits/stdc++.h>
+using namespace std;
+
+const int L = 256;
+int cnt[L];
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    memset(cnt, 0, sizeof cnt);
+
+    int n, m, l;
+    cin >> n >> m >> l;
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++) {
+            int x;
+            cin >> x;
+            cnt[x]++;
+        }
+
+    for (int i = 0; i < l; i++)
+        cout << cnt[i] << ' ';
+    cout << endl;
+
+    return 0;
+}
