0fdb508db51bb9bcda3ad1600eab7d905347111d
rucdatascience <87228246+rucdatascience@users.noreply.github.com>
2022-12-30 14:37:30 +0800

    1

--- 202209/202209_4.cpp
+++ 202209/202209_4.cpp
@@ -1,177 +1,192 @@
 ﻿/*
 CSP202209_4 吉祥物投票
 得分：100
+
+测试时n最大为1e9，这么大的数组超过了空间限制，必须把连续的投票意愿相同的人看作一个线段，用std::set维护线段树（区间树）。
 */
 #include<bits/stdc++.h>
 using namespace std;
 
-struct seg {
-    int id;
-    int l, r;
-    bool operator < (const seg b)const {
-        return l < b.l;
-    }
+int ID = 0;
+vector<int> parent_ID(200010), item_aID(200010), aID_item(200010);
+/*
+初始时，作品i有ID i，且parent_ID[i]=i。若parent_ID[i]=i，则i是一个ancestor ID（aID）。
+item_aID[x]=代表作品x的aID。aID_item[i]=一个aID i所代表的作品。
+
+操作一：每个线段投票有一个投票意愿ID i。
+通过改变parent_ID[i]来改变该线段所投的作品（并查集find[i]来找对应的aID）。
+
+操作二：parent_ID[item_aID[x]]=item_aID[w]，并给item_aID[x]一个新aID（之所以上面的大小为200010）。
+
+操作三：swap(item_aID[x], item_aID[y])
+
+如果不适用上述并查集方法会超时！
+*/
+
+struct node1 { // 用来维护作品投票排序
+	int item_name, votes_num;
 };
-struct Node {
-    int id, cnt;
-    bool operator > (const Node b)const {
-        if (cnt == b.cnt) return id < b.id;
-        return cnt > b.cnt;
-    }
+struct compare1 { // 函数对象，用以将node1按如下规则排序：先按votes_num从大到小排序，votes_num相等时按item_name从小到大排序
+	bool operator()(const node1& a, const node1& b)const { // set要求此处为常类型
+		if (a.votes_num == b.votes_num) {
+			return a.item_name < b.item_name; // item_id from small to large
+		}
+		else {
+			return a.votes_num > b.votes_num; // votes_num from large to small
+		}
+	}
 };
-int n, m, q, idx, op;
-int tot[100010];
-int p[200010], h[200010], pre[200010];
-set<seg> se;
-set<Node, greater<Node> > heap;
 
-void init() {//初始化
-    tot[0] = n;
-    se.insert({ 0,1,n });
-    for (int i = 0; i <= m; i++) {
-        p[i] = h[i] = pre[i] = i;
-        heap.insert({ i,tot[i] });
-    }
-    idx = m + 1;
-}
+struct node2 { // 线段投票
+	int left, right, ID;
+};
+struct compare2 { // 函数对象，用以将node2按left从小到大的规则排序
+	bool operator()(const node2& a, const node2& b)const { // set要求此处为常类型
+		return a.left < b.left; // left from small to large
+	}
+};
 
-int find(int x) {
-    if (x != p[x]) p[x] = find(p[x]);
-    return p[x];
-}
+int find_aID(int x); // 寻找一个ID的aID（并查集的find方法，带路径压缩）
+void option1(int l, int r, int x);
+void option2(int x, int w);
+void option3(int x, int y);
+void option4(int w);
+void option5();
 
-int mid(int x) {//通过编号找到任一个id
-    return h[x];
-}
-
-int idm(int x) {//通过id找到唯一的编号
-    return pre[find(x)];
-}
-
-void modify(int x, int l, int r) {//操作1
-    auto ita = se.upper_bound({ 0,l,0 });
-    auto itb = se.upper_bound({ 0,r,0 });
-    ita--;
-    // cout<<(*ita).l<<(*ita).r<<endl;
-    vector<seg> win;//待新插入的节点
-    vector<set<seg>::iterator> wer;//待删除的节点
-    win.push_back({ mid(x),l,r });
-    auto temp = heap.find({ x,tot[x] });
-    heap.erase(temp);
-    tot[x] += (r - l + 1);
-    heap.insert({ x,tot[x] });
-    for (auto it = ita; it != itb; it++) wer.push_back(it);
-    for (int i = 0; i < wer.size(); i++) {
-        seg t = *wer[i];
-        se.erase(t);
-        if (l >= t.l && r <= t.r) {//[t.l  [l  r]    t.r]的情况
-            if (l - 1 >= t.l) win.push_back({ t.id,t.l,l - 1 });
-            if (r + 1 <= t.r) win.push_back({ t.id,r + 1,t.r });
-            Node tc = *heap.find({ idm(t.id),tot[idm(t.id)] });
-            heap.erase(tc);
-            tot[tc.id] -= (r - l + 1);
-            heap.insert({ tc.id,tot[tc.id] });
-        }
-        else if (l <= t.r && t.l <= l) {//[t.l  [l   t.r]   r]的情况
-            if (l - 1 >= t.l) win.push_back({ t.id,t.l,l - 1 });
-            Node tc = *heap.find({ idm(t.id),tot[idm(t.id)] });
-            heap.erase(tc);
-            tot[tc.id] -= (t.r - l + 1);
-            heap.insert({ tc.id,tot[tc.id] });
-        }
-        else if (r <= t.r && t.l <= r) {//[l    [t.l   r]  t.r]的情况
-            if (r + 1 <= t.r) win.push_back({ t.id,r + 1,t.r });
-            Node tc = *heap.find({ idm(t.id),tot[idm(t.id)] });
-            heap.erase(tc);
-            tot[tc.id] -= (r - t.l + 1);
-            heap.insert({ tc.id,tot[tc.id] });
-        }
-        else if (l<t.l && r>t.r) {//[l  [t.l  t.r]    r]的情况
-            Node tc = *heap.find({ idm(t.id),tot[idm(t.id)] });
-            heap.erase(tc);
-            tot[tc.id] -= (t.r - t.l + 1);
-            heap.insert({ tc.id,tot[tc.id] });
-        }
-    }
-    for (int i = 0; i < win.size(); i++) se.insert(win[i]);
-}
-
-void exchange(int x, int y) {//操作3
-    heap.erase({ x,tot[x] }), heap.erase({ y,tot[y] });
-    int a = h[x], b = h[y];
-    pre[a] = y, pre[b] = x;
-    swap(h[x], h[y]);
-    swap(tot[x], tot[y]);
-    heap.insert({ x,tot[x] });
-    heap.insert({ y,tot[y] });
-}
-
-void xtoy(int x, int y) {//操作2
-    heap.erase({ x,tot[x] }), heap.erase({ y,tot[y] });
-    p[h[x]] = h[y];
-    p[idx] = idx;
-    h[x] = idx;
-    pre[idx++] = x;
-    tot[y] += tot[x];
-    tot[x] = 0;
-    heap.insert({ x,tot[x] });
-    heap.insert({ y,tot[y] });
-}
-
-void debug() {//没用不用管
-    cout << "--------------------------------------" << endl;
-    for (auto it = se.begin(); it != se.end(); it++) {
-        seg t = *it;
-        cout << t.l << " " << t.r << " " << idm(t.id) << "          " << tot[idm(t.id)] << endl;
-    }
-    cout << "--------------------------------------" << endl;
-
-    // cout<<"--------------------------------------"<<endl;
-    // for(auto it=heap.begin();it!=heap.end();it++){
-    //     Node t=*it;
-    //     cout<<t.cnt<<" "<<tot[t.id]<<" "<<t.id<<endl;
-    // }
-    // cout<<"--------------------------------------"<<endl;
-}
+vector<int> votes(100001, 0); // 每件作品的票数，初始为0
+set<node1, compare1> votes_rank; // 降序地记录作品的票数，用以快速查询最大票数的作品
+set<node2, compare2> votes_segment_tree; // 投票区间树
 
 int main() {
-    cin >> n >> m >> q;
-    init();
-    while (q--) {
-        // debug();
-        cin >> op;
-        if (op == 1) {
-            int l, r, x;
-            cin >> l >> r >> x;
-            modify(x, l, r);
-        }
-        else if (op == 2) {
-            int x, y;
-            cin >> x >> y;
-            xtoy(x, y);
-        }
-        else if (op == 3) {
-            int x, y;
-            cin >> x >> y;
-            exchange(x, y);
-        }
-        else if (op == 4) {
-            int x;
-            cin >> x;
-            cout << tot[x] << endl;
-        }
-        // else if(op==9){
-        //     int x;
-        //     cin>>x;
-        //     cout<<"--->"<<idm(x)<<endl;
-        // }
-        else {
-            if (tot[0] == n) cout << 0 << endl;
-            else {
-                if ((*heap.begin()).id) cout << (*heap.begin()).id << endl;
-                else cout << (*(++heap.begin())).id << endl;
-            }
-        }
-    }
-    system("pause");
+	int n, m, q;
+	cin >> n >> m >> q;
+	votes[0] = n;
+	votes_rank.insert({ 0,n });
+	votes_segment_tree.insert({ 1,n,ID }); // 所有人投票作品0（没有投票）
+	while (ID <= m) {
+		parent_ID[ID] = item_aID[ID] = aID_item[ID] = ID; // 初始化ID信息
+		ID++;
+	}
+
+	while (q--) {
+		int option;
+		cin >> option;
+		if (option == 1) {
+			int l, r, x;
+			cin >> l >> r >> x;
+			option1(l, r, x);
+		}
+		else if (option == 2) {
+			int x, w;
+			cin >> x >> w;
+			option2(x, w);
+		}
+		else if (option == 3) {
+			int x, y;
+			cin >> x >> y;
+			option3(x, y);
+		}
+		else if (option == 4) {
+			int w;
+			cin >> w;
+			option4(w);
+		}
+		else if (option == 5) {
+			option5();
+		}
+	}
+}
+
+int find_aID(int x) { // 寻找一个ID的aID
+	if (x != parent_ID[x]) parent_ID[x] = find_aID(parent_ID[x]); // 路径压缩，见并查集相关知识
+	return parent_ID[x];
+}
+
+void option1(int l, int r, int x) {
+	vector<node2> to_be_deleted, to_be_inserted; // 记录需要去除和添加的线段（在下面用指针遍历votes_segment_tree的过程中不能修改votes_segment_tree）
+	node2 a = { r,1,1 };
+	auto it_begin = votes_segment_tree.upper_bound(a); // 寻找left值大于r的第一个已有线段
+	if (it_begin == votes_segment_tree.end()) {
+		it_begin--; // 若寻找到了线段树的end处（votes_segment_tree.end()是最后一个线段的后一个无效node），则it_begin指向最后一个线段
+	}
+	for (auto it = it_begin;; it--) { // 向left值更小的方向遍历线段，去除与[l,r]重叠的线段
+		if (it->left > r) {
+			continue; // 该线段与[l,r]不重叠
+		}
+		else {
+			if (it->right < l) {
+				break; // 该线段及left值更小的线段与[l,r]不重叠
+			}
+			to_be_deleted.push_back(*it); // 该线段与[l,r]（部分）重叠
+			if (it->left < l) {
+				to_be_inserted.push_back({ it->left , l - 1, it->ID }); // 将该线段与[l,r]不重叠的部分生成新线段
+			}
+			if (it->right > r) {
+				to_be_inserted.push_back({ r + 1 , it->right, it->ID }); // 将该线段与[l,r]不重叠的部分生成新线段
+			}
+		}
+		if (it == votes_segment_tree.begin()) {
+			break; // 遍历完线段
+		}
+	}
+	for (auto it = to_be_deleted.begin(); it != to_be_deleted.end(); it++) {
+		votes_segment_tree.erase(*it);
+		int item_name = aID_item[find_aID(it->ID)];
+		votes[item_name] -= it->right - it->left + 1; // 去除线段并更新votes_rank、votes信息
+		votes_rank.insert({ item_name, votes[item_name] });
+	}
+	for (auto it = to_be_inserted.begin(); it != to_be_inserted.end(); it++) {
+		votes_segment_tree.insert(*it);
+		int aID = find_aID(it->ID);
+		int item_name = aID_item[aID];
+		votes[item_name] += it->right - it->left + 1; // 添加线段并更新votes_rank、votes信息
+		votes_rank.insert({ item_name, votes[item_name] });
+	}
+
+	votes_segment_tree.insert({ l,r,item_aID[x] }); // 添加线段[l,r]并更新votes_rank、votes信息
+	votes_rank.erase({ x, votes[x] });
+	votes[x] += r - l + 1; // 假设[l,r]之前没投x
+	votes_rank.insert({ x, votes[x] });
+}
+
+void option2(int x, int w) {
+	votes[w] += votes[x]; // 更新votes_rank、votes信息
+	votes[x] = 0;
+	votes_rank.insert({ w, votes[w] });
+
+	parent_ID[item_aID[x]] = item_aID[w]; // 更新parent_ID、item_aID、aID_item信息
+	item_aID[x] = ID; //  赋予x一个新的aID（以前的aID指向w的aID了）
+	parent_ID[ID] = ID;
+	aID_item[ID] = x;
+	ID++;
+}
+
+void option3(int x, int y) {
+	swap(votes[x], votes[y]); // 更新votes_rank、votes信息
+	votes_rank.insert({ x, votes[x] });
+	votes_rank.insert({ y, votes[y] });
+
+	swap(item_aID[x], item_aID[y]); // 更新item_aID、aID_item信息
+	aID_item[item_aID[x]] = x;
+	aID_item[item_aID[y]] = y;
+}
+
+void option4(int w) {
+	cout << votes[w] << endl;
+}
+
+void option5() {
+	/*votes_rank可能包含过大的错误票数*/
+	while (votes_rank.size() > 0) {
+		int item = votes_rank.begin()->item_name;
+		int votes_num = votes_rank.begin()->votes_num;
+		if (votes[item] == votes_num && item && votes_num) { // 此记录的票数是对的，且是票数为正的非0作品
+			cout << item << endl;
+			return;
+		}
+		else {
+			votes_rank.erase(votes_rank.begin()); // 删除无效记录
+		}
+	}
+	cout << 0 << endl; // votes_rank.size() == 0
 }
---
+++ 202209/202209_4_55.cpp
@@ -0,0 +1,188 @@
+﻿/*
+CSP202209_4 吉祥物投票
+得分：55
+
+测试时n最大为1e9，这么大的数组超过了空间限制，所以考虑把连续的投票意愿相同的人看作一个线段，用std::set维护线段树。
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int vote_ID = 0;
+map<int, int> vote_id_to_item_id;
+vector<set<int>> vote_ids_for_items(100000 + 1);
+
+struct node1 {
+	int item_id, votes_num;
+};
+struct compare1 { // 函数对象
+	bool operator()(const node1& a, const node1& b)const { // set要求此处为常类型
+		if (a.votes_num == b.votes_num) {
+			return a.item_id < b.item_id; // item_id from small to large
+		}
+		else {
+			return a.votes_num > b.votes_num; // votes_num from large to small
+		}
+	}
+};
+
+struct node2 {
+	int left, right, vote_id;
+};
+struct compare2 { // 函数对象
+	bool operator()(const node2& a, const node2& b)const { // set要求此处为常类型
+		return a.left < b.left; // left from small to large
+	}
+};
+
+void option1(int l, int r, int x);
+void option2(int x, int w);
+void option3(int x, int y);
+void option4(int w);
+void option5();
+
+vector<int> votes(100000 + 1, 0); // 每件作品的票数
+set<node1, compare1> votes_rank; // 有序地记录作品的票数（里面的元素和votes对应上才为真）
+set<node2, compare2> votes_segment_tree;
+
+int main() {
+	int n, m, q;
+	cin >> n >> m >> q;
+	votes[0] = n;
+	votes_rank.insert({ 0,n });
+	votes_segment_tree.insert({ 1,n, vote_ID });
+	vote_id_to_item_id[vote_ID] = 0;
+	vote_ids_for_items[0].insert(vote_ID);
+	vote_ID++;
+
+	while (q--) {
+		int option;
+		cin >> option;
+		if (option == 1) {
+			int l, r, x;
+			cin >> l >> r >> x;
+			option1(l, r, x);
+		}
+		else if (option == 2) {
+			int x, w;
+			cin >> x >> w;
+			option2(x, w);
+		}
+		else if (option == 3) {
+			int x, y;
+			cin >> x >> y;
+			option3(x, y);
+		}
+		else if (option == 4) {
+			int w;
+			cin >> w;
+			option4(w);
+		}
+		else if (option == 5) {
+			option5();
+		}
+	}
+}
+
+void option1(int l, int r, int x) {
+	vector<node2> to_be_deleted, to_be_inserted;
+	node2 a = { r,1,1 };
+	auto it_begin = votes_segment_tree.upper_bound(a);
+	if (it_begin == votes_segment_tree.end()) {
+		it_begin--;
+	}
+	//cout << "votes_segment_tree.size():" << votes_segment_tree.size() << endl;
+	//cout << "it_begin:" << it_begin->left << " " << it_begin->right << " " << it_begin->vote_id << endl;
+	for (auto it = it_begin;; it--) {
+		if (it->left > r) {
+			continue;
+		}
+		else {
+			if (it->right < l) {
+				break;
+			}
+			to_be_deleted.push_back(*it);
+			if (it->left < l) {
+				to_be_inserted.push_back({ it->left , l - 1, it->vote_id });
+			}
+			if (it->right > r) {
+				to_be_inserted.push_back({ r + 1 , it->right, it->vote_id });
+			}
+		}
+		if (it == votes_segment_tree.begin()) {
+			break;
+		}
+	}
+	for (auto it = to_be_deleted.begin(); it != to_be_deleted.end(); it++) {
+		//cout << "to_be_deleted:" << it->left << " " << it->right << " " << it->vote_id << endl;
+		votes_segment_tree.erase(*it);
+		int item_id = vote_id_to_item_id[it->vote_id];
+		votes[item_id] -= it->right - it->left + 1;
+		votes_rank.insert({ item_id, votes[item_id] });
+	}
+	for (auto it = to_be_inserted.begin(); it != to_be_inserted.end(); it++) {
+		//cout << "to_be_inserted:" << it->left << " " << it->right << " " << it->vote_id << endl;
+		votes_segment_tree.insert(*it);
+		int item_id = vote_id_to_item_id[it->vote_id];
+		votes[item_id] += it->right - it->left + 1;
+		votes_rank.insert({ item_id, votes[item_id] });
+	}
+
+	votes_segment_tree.insert({ l,r,vote_ID });
+	votes[x] += r - l + 1; // 假设[l,r]之前没投x
+	votes_rank.insert({ x, votes[x] });
+	vote_id_to_item_id[vote_ID] = x;
+	vote_ids_for_items[x].insert(vote_ID);
+	vote_ID++;
+}
+
+void option2(int x, int w) {
+	/*update votes and votes_rank*/
+	votes_rank.erase({ w, votes[w] });
+	votes[w] += votes[x];
+	votes[x] = 0;
+	votes_rank.insert({ w, votes[w] });
+	
+	for (auto it = vote_ids_for_items[x].begin(); it != vote_ids_for_items[x].end(); it++) {
+		vote_ids_for_items[w].insert(*it);
+		vote_id_to_item_id[*it] = w;
+	}
+	vote_ids_for_items[x].clear();
+}
+
+void option3(int x, int y) {
+	/*update votes and votes_rank*/
+	votes_rank.erase({ x, votes[x] });
+	votes_rank.erase({ y, votes[y] });
+	swap(votes[x], votes[y]);
+	votes_rank.insert({ x, votes[x] });
+	votes_rank.insert({ y, votes[y] });
+
+	auto middle2 = vote_ids_for_items[x];
+	vote_ids_for_items[x] = vote_ids_for_items[y];
+	vote_ids_for_items[y] = middle2;
+	for (auto it = vote_ids_for_items[x].begin(); it != vote_ids_for_items[x].end(); it++) {
+		vote_id_to_item_id[*it] = x;
+	}
+	for (auto it = vote_ids_for_items[y].begin(); it != vote_ids_for_items[y].end(); it++) {
+		vote_id_to_item_id[*it] = y;
+	}
+}
+
+void option4(int w) {
+	cout << votes[w] << endl;
+}
+
+void option5() {
+	while (votes_rank.size() > 0) {
+		int item = votes_rank.begin()->item_id;
+		int votes_num = votes_rank.begin()->votes_num;
+		if (votes[item] == votes_num && item && votes_num) { // this is a true record
+			cout << item << endl;
+			return;
+		}
+		else {
+			votes_rank.erase(votes_rank.begin()); // remove the false record
+		}
+	}
+	cout << 0 << endl; // votes_rank.size() == 0
+}
