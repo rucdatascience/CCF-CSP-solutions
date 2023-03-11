deca8ee021607934cf0e22ba05ce0deacf2bc880
YahuiSun <yahuisun@ruc.edu.cn>
2023-03-11 16:53:03 +0800

    Update enumeration_bit_computation.cpp

--- enumeration_bit_computation.cpp
+++ enumeration_bit_computation.cpp
@@ -15,7 +15,7 @@ i: 0...2^2-1
 
 int main()
 {
-    for (int i = 0; i < pow(2, 2); i++) { // 2^2 choices
+    for (int i = 0; i < (1 << 2); i++) { // 2^2 choices
         for (int j = 0; j < 2; j++) { // 每个choice的两位（每一位是0或1）
             if (i & (1 << j)) { // 位运算
                 cout << 1 << " ";
