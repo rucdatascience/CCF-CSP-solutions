8fea29c1c14268acbb8bd5ba3ec9bf682cbc4408
YahuiSun <yahuisun@ruc.edu.cn>
2023-03-18 14:17:20 +0800

    Create bitset_example.cpp

---
+++ bitset_example.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <bitset>
+using namespace std;
+
+int main()
+{
+	bitset<4> a1{ "0011" };
+	bitset<4> a2 = 4; // 0100
+	bitset<4> a3;
+	a3[0] = 0, a3[1] = 1, a3[2] = 0, a3[3] = 1; // 1010   a3[0]是最右端的一位
+
+	cout << a1 << " " << a2 << " " << a3 << " " << a1.count() << " " << a1.size() << endl;
+	cout << (a1 & a2) << " " << (a1 & a3) << endl; //与运算
+	cout << (a1 | a2) << " " << (a1 | a3) << endl; //或运算
+	cout << (a1 ^ a2) << " " << (a1 ^ a3) << endl; //异或运算
+	cout << ~a1 << " " << ~a2 << endl; //取反运算
+	cout << (a1 >> 2) << " " << (a2 << 1) << endl; //移位运算
+}
