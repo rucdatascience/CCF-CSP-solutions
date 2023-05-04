02bbe6cc8e8b5e08635757503127db73532112d8
rucdatascience <87228246+rucdatascience@users.noreply.github.com>
2023-05-04 15:53:58 +0800

    Update 202109_3.cpp

--- 202109/202109_3.cpp
+++ 202109/202109_3.cpp
@@ -1,6 +1,6 @@
 ﻿/*
 CSP202109_3 脉冲神经网络
-得分：100
+得分：100 （使用编译环境2满分，使用编译环境2、3超时）
 
 http://www.manongjc.com/detail/32-jplehuuexcqszce.html
 
@@ -17,7 +17,7 @@ double dt;
 int h[maxN], e[maxN], D[maxN], ne[maxN], idx = 0;
 double W[maxN], v[maxN], u[maxN], a[maxN], b[maxN], c[maxN], d[maxN];
 int r[maxN], cnt[maxN];
-double I[1024][maxN / 2];
+double I[1024][maxN / 2]; // N*D的滚动数组
 
 static unsigned long _next = 1;
 
@@ -29,7 +29,8 @@ int myrand(void) {
 
 int main()
 {
-    memset(h, -1, sizeof h);
+    ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    memset(h, -1, sizeof h); //h初值-1
 
     /*input*/
     cin >> N >> S >> P >> T;
@@ -54,16 +55,20 @@ int main()
         double c;
         cin >> a >> b >> c >> d;
         e[idx] = b, W[idx] = c, D[idx] = d, ne[idx] = h[a], h[a] = idx++;
-        mod = max(mod, d + 1);
+        /*
+        a是入结点，e[idx]表示这个突触连接的出结点b，W[idx]与D[idx]表示这个突触的脉冲强度和传播延迟，h[a]表示a向后的突触ID（idx），
+        因为a后可能有多个突触，使用ne[idx]存储上一个a后的突触ID，这样使用递归方式（直到ID变为-1）可以查询所有a后的突触ID
+        */
+        mod = max(mod, d + 1); // mod为最大的传播延迟+1
     }
 
     for (int i = 0; i < T; i++)
     {
-        int t = i % mod;
+        int t = i % mod; //利用对mod的取余来使用滚动数组I
         for (int j = N; j < N + P; j++)
-            if (r[j] > myrand())
+            if (r[j] > myrand()) //脉冲源在每个时刻以一定的概率发放一个脉冲
             {
-                for (int k = h[j]; ~k; k = ne[k]) // ~是位运算的取反运算符
+                for (int k = h[j]; ~k; k = ne[k]) // k为j后的所有突触，~是位运算的取反运算符(-1的取反是0)
                 {
                     int x = e[k];
                     I[(t + D[k]) % mod][x] += W[k];
@@ -82,7 +87,7 @@ int main()
                     int x = e[k];
                     I[(t + D[k]) % mod][x] += W[k];
                 }
-                cnt[j] ++;
+                cnt[j] ++; //cnt存储神经元的脉冲次数
                 v[j] = c[j], u[j] += d[j];
             }
         }
